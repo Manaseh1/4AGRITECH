9b66032edfc1510053a15ef82a018f31d4f6eb4e
Betsy Rotich <bc_rotich@kabarak.ac.ke>
2023-10-21 16:17:44 +0300

    price.php

---
+++ price.php
@@ -0,0 +1,41 @@
+<?php 
+include("./navbar.php"); 
+?>
+
+<div class="row">
+<table class="container striped centered" style="max-width: 500px;">
+        <th>
+          <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Price (ksh)</th>
+          </tr>
+        </th>
+
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Maize</td>
+            <td>120</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Beans</td>
+            <td>300</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Irish Potatoes</td>
+            <td>200</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>Wheat</td>
+            <td>300</td>
+          </tr>
+        </tbody>
+      </table>
+</div>
+
+
+<?php include("footer.php"); ?>
