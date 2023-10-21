f9d5934c7f9cdaa8712c159a903f529dabf1b783
chesire <kibichiigilbert024@gmailcom>
2023-10-21 15:02:15 +0300

    Frontend

---
+++ crops.php
@@ -0,0 +1,42 @@
+<?php include("./navbar.php"); ?>
+
+
+<table class="container striped centered" style="max-width: 500px;">
+        <thead>
+          <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Estimate (input) budget (ksh)</th>
+              <th>more Info</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Maize</td>
+            <td>$0.87</td>
+            <td><a href="moreinfo.php">more info</a></td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Maize</td>
+            <td>$0.87</td>
+            <td><a href="moreinfo.php">more info</a></td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Maize</td>
+            <td>$0.87</td>
+            <td><a href="moreinfo.php">more info</a></td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Maize</td>
+            <td>$0.87</td>
+            <td><a href="moreinfo.php">more info</a></td>
+          </tr>
+        </tbody>
+      </table>
+
+<?php include("footer.php"); ?>
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+  include("./navbar.php");
+?>  
+
+      <br><br>
+    <div class="container">
+        
+           
+        <form class="col s6 form">
+            <div class="row">
+                <div class="input-field col s6">
+                    <input id="first_name" type="text" class="validate">
+                    <label for="first_name">First Name</label>
+                  </div>
+                <div class="input-field col s6">
+                    <input id="last_name" type="text" class="validate">
+                    <label for="last_name">Last Name</label>
+                </div>
+                <div class="input-field col s12">
+                    <input id="email" type="email" class="validate">
+                    <label for="email">Email</label>
+                  </div>
+                <div class="input-field col s12">
+                    <input id="password" type="password" class="validate">
+                    <label for="password">Password</label>
+                  </div>
+
+                  <div class="input-field col s12">
+                    <input id="confirm_password" type="password" class="validate">
+                    <label for="confirm_password">confirm_Password</label>
+                  </div>
+            </div>
+
+            <a class="waves-effect waves-light btn-large">Sign Up</a>
+        </form>
+    </div>
+   
+    
+    </div>
+    </div>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    
+</body>
+</html>
